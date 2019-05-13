ed6bd5f9315846100eb3533723096cc23271d6fd
aaronmorgenegg <aaronmorgenegg@gmail.com>
2019-05-12 20:03:11 -0600

    Adding more commands, starting to add help menu

--- version2_python/Parser/parser.py
+++ version2_python/Parser/parser.py
@@ -1,5 +1,8 @@
+from version2_python.Actors.status import Status
 from version2_python.Actors.unit import Unit
+from version2_python.Encounter.encounter import Encounter
 from version2_python.Parser.lexer import *
+from version2_python.help.help import printHelpMenu
 
 precedence = (
     ('left', 'PLUS', 'MINUS'),
@@ -10,10 +13,16 @@ precedence = (
 # dictionary of names
 names = {}
 
+encounter = Encounter()
+
 
 def p_statement_expr(t):
     'statement : expression'
     print(t[1])
+
+def p_statement_command(t):
+    'statement : command'
+    pass
 
 
 def p_expression_binop(t):
@@ -51,36 +60,123 @@ def p_expression_number(t):
     t[0] = t[1]
 
 
-def p_command_expression(t):
-    'command : expression'
-    print(t[1])
+# def p_command_save_encounter(t):
+#     'command : SAVE'
+#     global encounter
+#     encounter.file_manager.save(encounter, "latest_encounter")
+#
+#
+# def p_command_save_item(t):
+#     'command : SAVE NAME'
+#     global encounter
+#     data = encounter.lookupName(t[2])
+#     encounter.file_manager.save(data, t[2])
+#
+#
+# def p_command_load_encounter(t):
+#     'command : LOAD'
+#     global encounter
+#     encounter.file_manager.load("latest_encounter")
+#
+# def p_command_load_item(t):
+#     'command : LOAD NAME'
+#     global encounter
+#     data = encounter.file_manager.load(t[2])
+#     if isinstance(data, Unit):
+#         encounter.addUnit(data)
 
 
-def p_command_save_encounter(t):
-    'command : SAVE'
-    encounter.file_manager.save(encounter, "latest_encounter")
-
-
-def p_command_save_item(t):
-    'command : SAVE NAME'
-    data = encounter.lookupName(t[2])
-    encounter.file_manager.save(data, t[2])
-
-
-def p_command_load_encounter(t):
-    'command : LOAD'
+def p_command_next_turn(t):
+    'command : NEXTTURN'
     global encounter
-    encounter = encounter.file_manager.load("latest_encounter")
+    encounter.nextTurn()
 
-def p_command_load_item(t):
-    'command : LOAD NAME'
-    data = encounter.file_manager.load(t[2])
-    if isinstance(data, Unit):
-        encounter.addUnit(data)
+
+def p_command_reset(t):
+    'command : RESET'
+    global encounter
+    encounter = Encounter()
+
+
+def p_command_help(t):
+    'command : HELP'
+    printHelpMenu()
+
+
+def p_command_create_unit(t):
+    'command : CREATE unit'
+    global encounter
+    encounter.addUnit(t[2])
+
+
+def p_command_delete_unit(t):
+    'command : DELETE NAME'
+    global encounter
+    encounter.deleteUnit(t[2])
+
+
+def p_unit(t):
+    '''unit : NAME expression'''
+    t[0] = Unit(t[1], initiative=t[2])
+
+
+def p_status(t):
+    '''status : NAME expression'''
+    t[0] = Status(t[1], time_total=t[2])
+
+
+def p_command_edit_armor(t):
+    '''command : NAME ARMOR expression'''
+    global encounter
+    try:
+        unit = encounter.lookupName(t[1])
+        unit.armor = t[3]
+    except Exception:
+        print("Error looking up name {}".format(t[1]))
+
+
+def p_command_edit_health(t):
+    '''command : NAME HEALTH expression'''
+    global encounter
+    try:
+        unit = encounter.lookupName(t[1])
+        unit.hp = t[3]
+    except Exception:
+        print("Error looking up name {}".format(t[1]))
+
+
+def p_command_edit_initiative(t):
+    '''command : NAME INITIATIVE expression'''
+    global encounter
+    try:
+        unit = encounter.lookupName(t[1])
+        unit.initiative_mod = t[3]
+    except Exception:
+        print("Error looking up name {}".format(t[1]))
+
+
+def p_command_edit_note(t):
+    '''command : NAME NOTE NAME'''
+    global encounter
+    try:
+        unit = encounter.lookupName(t[1])
+        unit.note = t[3]
+    except Exception:
+        print("Error looking up name {}".format(t[1]))
+
+
+def p_command_edit_status(t):
+    '''command : NAME STATUS status'''
+    global encounter
+    try:
+        unit = encounter.lookupName(t[1])
+        unit.status.append(t[3])
+    except Exception:
+        print("Error looking up name {}".format(t[1]))
 
 
 def p_error(t):
-    print("Syntax error at '%s'" % t.value)
+    print("Syntax error at '%s'" % t)
 
 
 def startParser():
---
+++ version2_python/help/help.py
@@ -0,0 +1,7 @@
+
+def printHelpMenu():
+    print("-----HELP-----")
+    print("Calculator:")
+    print(" Enter any math equation or dice roll to evaluate it.")
+    print(" ex. 5+20/4")
+    print(" ex. 11d6+4")
